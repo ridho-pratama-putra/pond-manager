dd01c79bff6feab38066fc1928cbeddbb9cb7c7f
ridho <ridho.putra@btpn.com>
2022-06-10 00:15:12 +0700

    listener will listen to topic not yet consume sequentially per partition before move to other partition

---
+++ src/main/java/com/lotus/pond/manager/PondListener.java
@@ -0,0 +1,19 @@
+package com.lotus.pond.manager;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PondListener {
+
+    Logger logger = LoggerFactory.getLogger(PondListener.class);
+
+    @KafkaListener(topics = {"pond"}, groupId = "pond-manager")
+    public void listener(String value) {
+        logger.info("I got [{}]", value);
+    }
+}
---
+++ src/main/java/com/lotus/pond/manager/PondManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lotus.pond.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PondManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PondManagerApplication.class, args);
+	}
+
+}
